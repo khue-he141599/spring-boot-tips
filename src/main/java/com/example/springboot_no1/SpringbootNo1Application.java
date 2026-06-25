7599bf7553afd25c3155835280a9dff312a6dd7a
khue.nd <khue.nd@sharitekvn.com>
2026-06-25 11:07:11 +0000

    init source java spring tips

---
+++ src/main/java/com/example/springboot_no1/SpringbootNo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_no1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNo1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springboot_no1/entity/ProductEntity.java
@@ -0,0 +1,4 @@
+package com.example.springboot_no1.entity;
+
+public class ProductEntity {
+}
