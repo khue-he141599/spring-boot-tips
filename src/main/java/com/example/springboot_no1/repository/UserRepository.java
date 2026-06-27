782fbbaff4c5c1eefe1b1e1222c9694debd9de72
khue-he141599 <khuendhe1415999@gmail.com>
2026-06-28 00:36:53 +0700

    (N/A)(feature) connect db and use jpa and jpql

--- src/main/java/com/example/springboot_no1/repository/UserRepository.java
+++ src/main/java/com/example/springboot_no1/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.springboot_no1.repository;
 
 import com.example.springboot_no1.entity.user.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -34,4 +37,21 @@ public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpec
     //RAW JPQL -> extends : JpaSpecificationExecutor
     @Query("SELECT u from UserEntity u where u.id = (SELECT MAX(p.id) FROM UserEntity p)")
     UserEntity findMaxId();
+
+    @Query("SELECT u from UserEntity u where u.userName = ?1 AND u.userEmail = ?2")
+    List<UserEntity> getUserEntityBy(String userName, String userEmail);
+
+    @Query("SELECT u from UserEntity u where u.userName = :userName AND u.userEmail = :userEmail")
+    List<UserEntity> getUserEntityByTwo(@Param("userName") String userName,@Param("userEmail") String userEmail);
+
+    // update - delete
+    @Modifying
+    @Query("UPDATE UserEntity u set u.userName =: userName")
+    @Transactional
+    int updateUserName(@Param("userName") String userName);
+
+    // native query - get count user
+    @Query(value = "SELECT COUNT(u.id) from java_user_001", nativeQuery = true)
+    long getToalUser();
+
 }
